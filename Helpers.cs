829eba5a7a722cfbb35b51417883849dfafa34a0
Okoniewitz <37485333+Okoniewitz@users.noreply.github.com>
2024-01-25 15:57:11 +0100

    Dodaj pliki projektów.

---
+++ Helpers.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTA;
+using GTA.Native;
+using System.IO;
+
+namespace NotUselessArmor
+{
+    public static class Helpers
+    {
+        public static ScriptSettings config = ScriptSettings.Load("scripts\\Okoniewitz\\NotUselessArmor.ini");
+
+        public static void ConfigRead()
+        {
+            string[] ConfigLines =
+            { 
+                "[FEATURES]",
+                "DEGRADATION=false",
+                "VISIBLEARMOR=true",
+                "HEALING=true",
+                "LOCATIONALDAMAGE=true",
+                "RAGDOLLONDAMAGE=true",
+                "WRITHEDISABLED=true",
+                "[SETTINGS]",
+                "PUTOFFARMORONLYWHENDEAD=true",
+                "ARMORPERHEAL=3",
+                "DELAYPERHEAL=3000",
+                "DAMAGEDELAY=6000",
+                "RUNSHOOTJUMPDELAY=2000",
+                "RAGDOLLCHANCE=3",
+                "VISIBLEARMORTYPE=3",
+                "RAGDOLLTIME=1300",
+                "RAGDOLLMINIMALTIME=500",
+                "[DEBUG]",
+                "DEBUG=false",
+                "[SAVE]",
+                "PLAYER0ARMOR=false",
+                "PLAYER1ARMOR=false",
+                "PLAYER2ARMOR=false",
+                "[ARMOR]",
+                "MICHAELCOMPONENTID=8",
+                "MICHAELDRAWABLEID=17",
+                "MICHAELCLEARDRAWABLEID=0",
+                "FRANKLINCOMPONENTID=8",
+                "FRANKLINDRAWABLEID=6",
+                "FRANKLINCLEARDRAWABLEID=14",
+                "TREVORCOMPONENTID=8",
+                "TREVORDRAWABLEID=8",
+                "TREVORCLEARDRAWABLEID=14"
+            };
+            if(!File.Exists("scripts\\Okoniewitz\\NotUselessArmor.ini")) { File.WriteAllLines("scripts\\Okoniewitz\\NotUselessArmor.ini",ConfigLines); }
+
+            Main.Degradation = config.GetValue<bool>("FEATURES", "DEGRADATION", false);
+            Main.VisibleArmorEnabled = config.GetValue<bool>("FEATURES", "VISIBLEARMOR", true);
+            Main.HealingEnabled = config.GetValue<bool>("FEATURES", "HEALING", true);
+            Main.LocationalDamage = config.GetValue<bool>("FEATURES", "LOCATIONALDAMAGE", true);
+            Main.RagdollOnDamage = config.GetValue<bool>("FEATURES", "RAGDOLLONDAMAGE", true);
+            Main.WritheDisabled = config.GetValue<bool>("FEATURES", "WRITHEDISABLED", true);
+            Main.Debug = config.GetValue<bool>("DEBUG", "DEBUG", false);
+            Main.PutOffArmorOnlyWhenDead = config.GetValue<bool>("SETTINGS", "PUTOFFARMORONLYWHENDEAD", true);
+            Main.ApPerHeal = config.GetValue<int>("SETTINGS", "ARMORPERHEAL", 3);
+            Main.DelayTime = config.GetValue<int>("SETTINGS", "DELAYPERHEAL", 3000);
+            Main.DamageDelay = config.GetValue<int>("SETTINGS", "DAMAGEDELAY", 6000);
+            Main.RunShootJumpDelay = config.GetValue<int>("SETTINGS", "RUNSHOOTJUMPDELAY", 2000);
+            Main.RagdollChance = config.GetValue<int>("SETTINGS", "RAGDOLLCHANCE", 3);
+            Main.VisibleArmorType = config.GetValue<int>("SETTINGS", "VISIBLEARMORTYPE", 3);
+            Main.RagdollTime = config.GetValue<int>("SETTINGS", "RAGDOLLTIME", 1300);
+            Main.RagdollMinimalTime = config.GetValue<int>("SETTINGS", "RAGDOLLMINIMALTIME", 500);
+            Main.PlayerArmorWear = new bool[] {
+                config.GetValue<bool>("SAVE", "PLAYER0ARMOR", false),
+                config.GetValue<bool>("SAVE", "PLAYER1ARMOR", false),
+                config.GetValue<bool>("SAVE", "PLAYER2ARMOR", false)
+            };
+            Modeles = new int[3, 3]
+            {
+                { config.GetValue<int>("ARMOR", "MICHAELCOMPONENTID", 8),config.GetValue<int>("ARMOR", "MICHAELDRAWABLEID", 17), config.GetValue<int>("ARMOR", "MICHAELCLEARDRAWABLEID", 0)},
+                { config.GetValue<int>("ARMOR", "FRANKLINCOMPONENTID", 8),config.GetValue<int>("ARMOR", "FRANKLINDRAWABLEID", 6), config.GetValue<int>("ARMOR", "FRANKLINCLEARDRAWABLEID", 14)},
+                { config.GetValue<int>("ARMOR", "TREVORCOMPONENTID", 8),config.GetValue<int>("ARMOR", "TREVORDRAWABLEID", 8), config.GetValue<int>("ARMOR", "TREVORCLEARDRAWABLEID", 14)}
+            };
+    }
+
+        public static int GetArmorType()
+        {
+            int armor = Game.Player.Character.Armor;
+            if (armor > 80) return 5;
+            if (armor > 60) return 4;
+            if (armor > 40) return 3;
+            if (armor > 20) return 2;
+            if (armor > 0) return 1;
+            return 0;
+        }
+
+        public static int[] Armor =
+        {
+
+            (int)Bone.SkelLeftClavicle,
+            (int)Bone.SkelRightClavicle,
+            (int)Bone.SkelSpine0,
+            (int)Bone.SkelSpine1,
+            (int)Bone.SkelSpine2,
+            (int)Bone.SkelSpine3,
+            (int)Bone.SkelSpineRoot,
+            (int)Bone.SkelRoot,
+            (int)Bone.SkelPelvis,
+            (int)Bone.IKRoot,
+        };
+        public static int ArmorPlayer = 0;
+
+        public static bool CanRegenHP()
+        {
+            Ped Player = Game.Player.Character;
+            return !Player.IsSprinting && !Player.IsSwimming && !Player.IsRagdoll && !Player.IsJumping && !Player.IsClimbing && !Player.IsDiving && !Player.IsGettingUp && !Player.IsShooting;
+        }
+
+
+        public static bool Damaged(OutputArgument bone)
+        {
+            bool damaged = Function.Call<bool>(Hash.GET_PED_LAST_DAMAGE_BONE, Game.Player.Character, bone);
+            Function.Call(Hash.CLEAR_PED_LAST_DAMAGE_BONE, Game.Player.Character);
+            return damaged;
+        }
+
+        public static int GetPlayerID()
+        {
+            if (Game.Player.Character.Model.Hash == GetHashKey("PLAYER_TWO")) return 2;
+            if (Game.Player.Character.Model.Hash == GetHashKey("PLAYER_ONE")) return 1;
+            if (Game.Player.Character.Model.Hash == GetHashKey("PLAYER_ZERO")) return 0;
+            return 3;
+        }
+
+        private static int[,] Modeles;
+
+        public static void ShowArmor(bool Show)
+        {
+            if(Show) Function.Call(Hash.SET_PED_COMPONENT_VARIATION, Game.Player.Character, Modeles[GetPlayerID(), 0], Modeles[GetPlayerID(), 1], 0);
+            else Function.Call(Hash.SET_PED_COMPONENT_VARIATION, Game.Player.Character, Modeles[GetPlayerID(), 0], Modeles[GetPlayerID(), 2], 0);
+        }
+
+        private static int GetHashKey(string value)
+        {
+            return Function.Call<int>(Hash.GET_HASH_KEY, value);
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTA;
+using GTA.Native;
+using GTA.Math;
+using System.IO;
+using System.Windows.Forms;
+using GTA.NaturalMotion;
+using System.Drawing;
+
+namespace NotUselessArmor
+{
+    public class Main : Script
+    {
+        public Main()
+        {
+            Helpers.ConfigRead();
+            Tick += OnTick;
+            if(WritheDisabled) Tick += NoWrithe;
+            if (Debug) Tick += DebugTick;
+        }
+
+        public static int ApPerHeal, DelayTime, DamageDelay, RunShootJumpDelay, RagdollChance, VisibleArmorType, RagdollTime, RagdollMinimalTime;
+        public static bool Degradation, VisibleArmorEnabled, PutOffArmorOnlyWhenDead, HealingEnabled, LocationalDamage, RagdollOnDamage, WritheDisabled;
+        public static bool Debug=true;
+        public static bool[] PlayerArmorWear;
+
+
+        private static int ArmorType;
+        private static bool Dead, NoArmor;
+        private static int HealDelay;
+
+        public static void DebugTick(object sender, EventArgs e)
+        {
+            string ArmorWearString;
+            if (VisibleArmorEnabled) ArmorWearString = "\nP0: " + PlayerArmorWear[0] + " P1: " + PlayerArmorWear[1] + " P2: " + PlayerArmorWear[2] + " PutOffOnlyWhenDead: " + PutOffArmorOnlyWhenDead; else ArmorWearString = "\nArmorWear Disabled";
+            GTA.UI.Screen.ShowSubtitle("Armor type: " + ArmorType + " Degradation: " + Degradation + " Armor: " + Game.Player.Character.Armor + "\nHealing: " + HealingEnabled + " Delay: " + (Math.Round((double)(HealDelay - Game.GameTime) / 100) / 10) + "s Can Regen: " + Helpers.CanRegenHP() + ArmorWearString);
+           
+            if (Game.IsKeyPressed(Keys.M))
+            {
+                Game.Player.Character.Armor++;
+            }
+            if (Game.IsKeyPressed(Keys.N))
+            {
+                Game.Player.Character.Armor--;
+            }
+        }
+
+        public static void NoWrithe(object sender, EventArgs e)
+        {
+           Ped[] peds = World.GetAllPeds();
+            foreach(Ped ped in peds)
+            {
+                if(ped.CanWrithe)ped.CanWrithe = false;
+            }
+        }
+
+        public static void OnTick(object sender, EventArgs e)
+        {
+            OutputArgument boneOut = new OutputArgument();
+            if (Game.Player.Character.Armor <= 0) NoArmor = true;
+            if (NoArmor && (Helpers.GetArmorType() == 1 || Helpers.GetArmorType() == 2)) { PlayerArmorWear[Helpers.GetPlayerID()] = false; NoArmor = false; }
+            if (Game.Player.Character.Armor > 0) NoArmor = false;
+            if (Helpers.GetArmorType() > ArmorType) { ArmorType = Helpers.GetArmorType(); if (ArmorType >= VisibleArmorType && VisibleArmorEnabled) PlayerArmorWear[Helpers.GetPlayerID()] = true; };
+            if (Game.Player.Character.Armor == 0) { ArmorType = 0; if (!PutOffArmorOnlyWhenDead) PlayerArmorWear[Helpers.GetPlayerID()] = false; }
+            if (Game.Player.Character.IsDead) Dead = true; else if (Dead) { PlayerArmorWear[Helpers.GetPlayerID()] = false; Dead = false; }
+
+            if (!Helpers.CanRegenHP() && HealDelay < Game.GameTime + RunShootJumpDelay) HealDelay = Game.GameTime +RunShootJumpDelay;
+            if (Helpers.Damaged(boneOut))
+            {
+                int rnd = new Random().Next(10);
+                HealDelay = Game.GameTime + DamageDelay;
+                if (Game.Player.Character.Armor > 0)
+                {
+                    if (!Helpers.Armor.Contains(boneOut.GetResult<int>()) && LocationalDamage)
+                    {
+                        int dif = Helpers.ArmorPlayer - Game.Player.Character.Armor;
+                        Game.Player.Character.Health -= dif;
+                        Game.Player.Character.Armor += dif;
+                        if (rnd <= RagdollChance && RagdollOnDamage)
+                        {
+                            Function.Call<bool>(Hash.SET_PED_TO_RAGDOLL, Game.Player.Character, RagdollTime, RagdollMinimalTime, 0, true, true, true);
+                        }
+                    }
+                }
+                else if (rnd <= RagdollChance && RagdollOnDamage)
+                {
+                    Function.Call<bool>(Hash.SET_PED_TO_RAGDOLL, Game.Player.Character, RagdollTime, RagdollMinimalTime, 0, true, true, true);
+                }
+            }
+            if (ArmorType > 0)
+            {
+                if (Degradation)
+                {
+                    if (Game.Player.Character.Armor <= (ArmorType - 1) * 20)
+                    {
+                        ArmorType = Helpers.GetArmorType();
+                    }
+                }
+
+                if (Game.Player.Character.Armor < ArmorType * 20 && HealDelay <= Game.GameTime && HealingEnabled)
+                {
+                    if (((ArmorType * 20) - Game.Player.Character.Armor) < ApPerHeal) Game.Player.Character.Armor += (ArmorType * 20) - Game.Player.Character.Armor;
+                    else
+                        Game.Player.Character.Armor += ApPerHeal;
+                    HealDelay = Game.GameTime + DelayTime;
+                }
+            }
+
+            if (VisibleArmorEnabled)
+            {
+                Helpers.ShowArmor(PlayerArmorWear[Helpers.GetPlayerID()]);
+            }
+            Helpers.ArmorPlayer = Game.Player.Character.Armor;
+
+            bool Saved = Helpers.config.GetValue<bool>("SAVE", "PLAYER" + Helpers.GetPlayerID() + "ARMOR", false);
+            Helpers.config.SetValue<bool>("SAVE", "PLAYER" + Helpers.GetPlayerID() + "ARMOR", PlayerArmorWear[Helpers.GetPlayerID()]);
+            if(Saved!=PlayerArmorWear[Helpers.GetPlayerID()])Helpers.config.Save();
+        }
+    }
+}
